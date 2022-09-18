9d6c4bade82c11cee9b39c3026eb67f399f29983
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-09-18 16:49:11 +0100

    adds state to SearchBar

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+import { BarSearch, FormSearch } from "./SearchBar.styled";
+
+export default class SearchBar extends Component {
+    state = {
+        inputFilter: '',
+    };
+}
