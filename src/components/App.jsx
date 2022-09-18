d43be18718a75532d746f75da9d0fed8130076ed
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-09-18 17:08:03 +0100

    adds state to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import SearchBar from "./SearchBar/SearchBar";
 
 class App extends Component {
+  state = {
+    inputFilter: '',
+  };
 
   render() {
     return(
