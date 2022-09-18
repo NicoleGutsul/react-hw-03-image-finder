1a5a16ebb329f9b222004106c65ba5c9d6670f1d
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-09-18 16:55:19 +0100

    creates handleInputChange in SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -5,4 +5,36 @@ export default class SearchBar extends Component {
     state = {
         inputFilter: '',
     };
+
+    handleInputChange = e => {
+        e.preventDefault();
+        if (this.state.inputFilter.trim() === '') {
+            alert('Введите название');
+            return;
+        }
+        this.props.onSubmit(this.state.inputFilter);
+        this.setState({inputFilter: ''});
+    };
+
+    render() {
+        return(
+            <BarSearch>
+        <FormSearch onSubmit={this.handleSubmit}>
+          <button type="submit" className="button">
+            Search
+          </button>
+
+          <input
+            className="input"
+            type="text"
+            value={this.state.inputFilter}
+            onChange={this.handleInputChange}
+            // autocomplete="off"
+            // autofocus
+            placeholder="Search images and photos"
+          />
+        </FormSearch>
+      </BarSearch>
+        )
+    }
 }
