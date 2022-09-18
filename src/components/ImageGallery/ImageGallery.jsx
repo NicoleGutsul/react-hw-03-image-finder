d7260003cbfbd3f3fe659520de5aeaf3bc0c9f14
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-09-18 17:35:43 +0100

    adds ImageGalleryItem to ImageGallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,11 +1,21 @@
 import { Component } from "react";
 import { GalleryImage } from "./ImageGallery.styled";
+import ImageGalleryItem from "components/ImageGalleryItem/ImageGalleryItem";
 
 export default class ImageGallery extends Component {
+    state = {
+        gallery: [],
+    };
     render() {
         return (
             <GalleryImage>
-                
+
+                {this.state.gallery === '' ? (
+                    <p>Enter name images!</p>
+                ) : (
+                    <ImageGalleryItem gallery={this.state.gallery}/>
+                )}
+
             </GalleryImage>
         )
     }
