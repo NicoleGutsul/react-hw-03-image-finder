6a0cc337ab85c8e2a75887810d19137d1ab2a9de
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-09-19 14:29:39 +0100

    adds state to ImageGallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,10 +1,19 @@
 import { Component } from "react";
 import { GalleryImage } from "./ImageGallery.styled";
 import ImageGalleryItem from "components/ImageGalleryItem/ImageGalleryItem";
+import { Circles } from "react-loader-spinner";
+import { FetchData } from "../../FetchData/FetchData";
 
 export default class ImageGallery extends Component {
     state = {
         gallery: [],
+        page: 1,
+        loading: false,
+        modal: false,
+        currentUrl: '',
+        currentTags: '',
+        total: 0,
+        totalHits: 0,
     };
     render() {
         return (
