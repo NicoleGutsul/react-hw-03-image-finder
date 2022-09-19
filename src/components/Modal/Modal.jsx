281abc3c6c5d7a939fa51fb86499ad7b552d0a70
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-09-19 15:01:10 +0100

    Modal componentDidMount

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,6 +2,17 @@ import { Component } from "react";
 import { Overlay, ModalList } from "./Modal.styled";
 
 export default class Modal extends Component {
+    componentDidMount() {
+        window.addEventListener('keydown', this.handleKeyDown);
+    }
+
+    handleKeyDown = e => {
+        if (e.code === 'Escape') {
+            this.props.onClose();
+        }
+
+    }
+
     render() {
         return (
             <Overlay>
