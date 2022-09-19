4b46281b4d85d2089157281695801131a354f08f
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-09-19 15:20:35 +0100

    adds toggleModal to ImageGallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,6 +3,7 @@ import { GalleryImage } from "./ImageGallery.styled";
 import ImageGalleryItem from "components/ImageGalleryItem/ImageGalleryItem";
 import { Circles } from "react-loader-spinner";
 import { ButtonLoadMore } from "../LoadMore/LoadMore";
+import Modal from "../Modal/Modal";
 import { FetchData } from "../../FetchData/FetchData";
 
 export default class ImageGallery extends Component {
@@ -16,6 +17,12 @@ export default class ImageGallery extends Component {
         total: 0,
         totalHits: 0,
     };
+
+    toggleModal = () => {
+        this.setState(state => ({
+            modal: !state.modal,
+        }));
+    }
 
     loadMore = () => {
         this.setState(prevState => ({ page: prevState.page + 1 }));
