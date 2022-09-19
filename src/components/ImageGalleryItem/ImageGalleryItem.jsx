ade9458449829acb4567aef23ac88f66609d3c57
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-09-19 15:49:56 +0100

    adds onClick to ImageGalleryItem

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,14 +1,18 @@
 import { GalleryItem, GalleryItemImage } from "./ImageGalleryItem.styled";
 
-const ImageGalleryItem = ({ gallery }) => {
+const ImageGalleryItem = ({ gallery, onClickModal, getImage }) => {
     return gallery.map(data => {
         return (
             <GalleryItem
               key={data.id}
+              onClick={() => {
+                onClickModal();
+                getImage(data.webformatURL, data.tags);
+              }}
             >
                 <GalleryItemImage
                   src={data.webformatURL}
-                  alt={DOMMatrixReadOnly.tags}
+                  alt={data.tags}
                   width="180"
                 />
             </GalleryItem>
